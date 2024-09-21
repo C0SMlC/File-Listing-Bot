38eefce819b44c152284832ea29aa8aaf2afe464
C0SMlC <pendurkarpratik04@gmail.com>
2024-09-21 14:49:28 +0530

    update info.py

--- info.py
+++ info.py
@@ -1,11 +1,17 @@
 import telegram
 from telegram.ext import Updater
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
 
 print(f"python-telegram-bot version: {telegram.__version__}")
 
 # Try to create an Updater instance
 try:
-    updater = Updater("7087859707:AAFhDQskswKPoK9wAm3IBhb0NCQNYHFhxIE")
+    updater = Updater(TOKEN)
     print("Updater attributes:", dir(updater))
 except Exception as e:
     print(f"Error creating Updater: {e}")
